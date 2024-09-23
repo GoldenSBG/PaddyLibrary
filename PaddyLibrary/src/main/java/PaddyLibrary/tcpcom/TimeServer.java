46189393c35b9c79b64ad75e4e6b4773c26a64a4
Golden_SBG <127042857+GoldenSBG@users.noreply.github.com>
2024-09-23 15:54:37 +0200

    TimeServer.java aktualisieren

--- PaddyLibrary/src/main/java/PaddyLibrary/tcpcom/TimeServer.java
+++ PaddyLibrary/src/main/java/PaddyLibrary/tcpcom/TimeServer.java
@@ -29,7 +29,7 @@ public class TimeServer implements TCPServerListener {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main() {
         new TimeServer();
     }
 }
