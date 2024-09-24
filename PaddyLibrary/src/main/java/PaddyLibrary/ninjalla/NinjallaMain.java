d9e9ca554352d9d1b0f1fd2bab2172fab8606a10
Golden_SBG <patrick.salvador@sunrise.ch>
2024-09-24 11:33:49 +0200

    Änderungen
    Ninjalla angefangen

--- PaddyLibrary/src/main/java/PaddyLibrary/ninjalla/NinjallaMain.java
+++ PaddyLibrary/src/main/java/PaddyLibrary/ninjalla/NinjallaMain.java
@@ -1,12 +1,8 @@
 package PaddyLibrary.ninjalla;
 
-import PaddyLibrary.snakeGame.GameBoard;
-
-import javax.swing.*;
-
 public class NinjallaMain {
 
-    public static void main(String[] args) {
+    public void start() {
     }
 
     public void pause() {
@@ -15,5 +11,11 @@ public class NinjallaMain {
 
     public void stop() {
         // Logik zum Stoppen des Spiels
+    }
+
+    private static void main(String[] args) {
+        /*
+        Testlogik in Java
+        */
     }
 }
